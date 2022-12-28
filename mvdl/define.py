5286d172b1020e49d0d05f9482193122e13cf306
ahron-maslin <aronmaslin@gmail.com>
2022-12-28 17:02:23 +0200

    cached credentials to speed up login

--- mvdl/define.py
+++ mvdl/define.py
@@ -11,22 +11,22 @@ LOGIN_PAYLOAD = 'data=%7B%22Name%22%3A%22{username}%22%2C%22Password%22%3A%22{pa
 DOWNLOAD_DIRECTORY = path.expanduser('~') + '/downloads/'
 QUALITIES = ['src_vip_hd_1080p', 'src_vip_hd', 'src_vip_sd', 'src_free_sd']
 HEADERS = {'POST': '/webapi/user/login.php HTTP/1.1',
-           'Host': 'mobilevids.org',
-           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0',
-           'Accept': '*/*',
-           'Accept-Language': 'en-US,en;q=0.5',
-           'Accept-Encoding': 'gzip, deflate, br',
-           'sec-ch-ua' : '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
-           'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-           'Cookie': 'menuNodes=[{"name":"Movies","icon":"mdi-movie","children":[{"name":"Browse","path":"/movies","icon":"mdi-view-list"},{"name":"Popular","path":"/top_movies","icon":"mdi-movie-filter"}]},{"name":"TV Shows","icon":"mdi-remote-tv","children":[{"name":"Browse ","path":"/tvshows","icon":"mdi-view-list"},{"name":"Popular ","path":"/top_shows","icon":"mdi-monitor-star"},{"name":"Calendar","path":"/show_calendar","icon":"mdi-calendar"}]}]',
-           'X-Requested-With': 'XMLHttpRequest',
-           'Content-Length': '77',
-           'Origin': 'https://mobilevids.org',
-           'Connection': 'keep-alive',
-           'Referer': 'https://mobilevids.org/legacy/',
-           'Sec-Fetch-Dest': 'empty',
-           'Sec-Fetch-Mode': 'cors',
-           'Sec-Fetch-Site': 'same-origin',
-           'sec-ch-ua-platform' : '"Windows"'
-           }
-
+						'Host': 'mobilevids.org',
+						'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0',
+						'Accept': '*/*',
+						'Accept-Language': 'en-US,en;q=0.5',
+						'Accept-Encoding': 'gzip, deflate, br',
+						'sec-ch-ua' : '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+						'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+						'Cookie': 'menuNodes=[{"name":"Movies","icon":"mdi-movie","children":[{"name":"Browse","path":"/movies","icon":"mdi-view-list"},{"name":"Popular","path":"/top_movies","icon":"mdi-movie-filter"}]},{"name":"TV Shows","icon":"mdi-remote-tv","children":[{"name":"Browse ","path":"/tvshows","icon":"mdi-view-list"},{"name":"Popular ","path":"/top_shows","icon":"mdi-monitor-star"},{"name":"Calendar","path":"/show_calendar","icon":"mdi-calendar"}]}]',
+						'X-Requested-With': 'XMLHttpRequest',
+						'Content-Length': '77',
+						'Origin': 'https://mobilevids.org',
+						'Connection': 'keep-alive',
+						'Referer': 'https://mobilevids.org/legacy/',
+						'Sec-Fetch-Dest': 'empty',
+						'Sec-Fetch-Mode': 'cors',
+						'Sec-Fetch-Site': 'same-origin',
+						'sec-ch-ua-platform' : '"Windows"'
+						}
+AUTH_TOKEN_CACHE = 'auth_token.txt'
